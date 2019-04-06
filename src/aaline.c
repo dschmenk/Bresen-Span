f2dfa11b6fc3f1149a1ba597d4861895454b260e
David Schmenk <dschmenk@gmail.com>
2019-04-05 19:17:20 -0700

    Fix alpha multiply tables and AA line horizontal/verical spans

--- src/aaline.c
+++ src/aaline.c
@@ -18,7 +18,7 @@ void aaline(int x1, int y1, int x2, int y2)
     {
         if (dy == 0)
         {
-            sx > 0 ? hspan(x1, x2, y1) : hspan(x2, x1, y1);
+            for (x2 += sx;x1 != x2; x1 += sx) aapixel(x1, y1, 0xFF);
             return;
         }
         inc           = ((long)dy << 16) / dx;
@@ -49,7 +49,7 @@ void aaline(int x1, int y1, int x2, int y2)
     {
         if (dx == 0)
         {
-            sy > 0 ? vspan(x1, y1, y2) : vspan(x1, y2, y1);
+            for (y2 += sy;y1 != y2; y1 += sy) aapixel(x1, y1, 0xFF);
             return;
         }
         inc           = ((long)dx << 16) / dy;
